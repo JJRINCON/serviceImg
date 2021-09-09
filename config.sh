1846d23ebc530306d5e417685fb4e1b7beab1189
Jhon Jairo Rincon <48601559+JJRINCON@users.noreply.github.com>
2021-09-09 12:01:27 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-sudo systemctl stop mysqld
-sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
-sudo systemctl restart mysqld
-mysql -u root mysql
-update user set authentication_string=password('@Sistemas2021') where user='root';
-flush privileges;
-exit;
-sudo systemctl unset-environment MYSQLD_OPTS
-sudo systemctl restart mysqld
+sudo apt-get update > /dev/null
+# Build Expect script
+tee ~/secure_our_mysql.sh > /dev/null << EOF
+spawn $(which sudo mysql_secure_installation)
+
+expect "Press y|Y for Yes, any other key for No:"
+send "n\r"
+
+expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
+send "0\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "New password:"
+send "@Sistemas2021\r"
+
+expect "Re-enter new password:"
+send "@Sistemas2021\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+EOF
