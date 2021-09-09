4265762372d92fac625fa7e8d02464c54d4642ea
Jhon Jairo Rincon <48601559+JJRINCON@users.noreply.github.com>
2021-09-08 23:11:05 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+sudo systemctl stop mysqld
+sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+sudo systemctl restart mysqld
+mysql -u root mysql
+update user set authentication_string=password('@Sistemas2021') where user='root';
+flush privileges;
+exit;
+sudo systemctl unset-environment MYSQLD_OPTS
+sudo systemctl restart mysqld
