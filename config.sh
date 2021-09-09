1fccfc3bd4457758d38ac794bcae962f817fe303
Jhon Jairo Rincon <48601559+JJRINCON@users.noreply.github.com>
2021-09-09 12:19:14 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+MYSQL_ROOT_PASSWORD='@Sistemas2021' # SET THIS! Avoid quotes/apostrophes in the password, but do use lowercase + uppercase + numbers + special chars
+
+# Install MySQL
+echo debconf mysql-server/root_password password $MYSQL_ROOT_PASSWORD | sudo debconf-set-selections
+echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD | sudo debconf-set-selections
+sudo apt-get -qq install mysql-server expect # > /dev/null # Install MySQL quietly
+
+echo "MySQL Root Password: $MYSQL_ROOT_PASSWORD\r"
+
 sudo apt-get update > /dev/null
 # Build Expect script
 tee ~/secure_our_mysql.sh > /dev/null << EOF
-spawn $(which sudo mysql_secure_installation)
-
+spawn $(which mysql_secure_installation)
+expect "Enter password for user root:"
+send "$MYSQL_ROOT_PASSWORD\r"
 expect "Press y|Y for Yes, any other key for No:"
 send "n\r"
 
@@ -14,10 +24,10 @@ expect "Change the password for root ? ((Press y|Y for Yes, any other key for No
 send "y\r"
 
 expect "New password:"
-send "@Sistemas2021\r"
+send "$MYSQL_ROOT_PASSWORD\r"
 
 expect "Re-enter new password:"
-send "@Sistemas2021\r"
+send "$MYSQL_ROOT_PASSWORD\r"
 
 expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
@@ -30,5 +40,13 @@ send "y\r"
 
 expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 EOF
+
+# Run Expect script.
+# This runs the "mysql_secure_installation" script which removes insecure defaults.
+sudo expect ~/secure_our_mysql.sh
+
+/etc/init.d/mysql restart
+
+# Cleanup
+rm -v ~/secure_our_mysql.sh # Remove the generated Expect script
